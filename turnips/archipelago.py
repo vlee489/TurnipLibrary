a65cce862362ee5257a9354e46e7c7985bdb9609
nago <nago@trimex.us>
2020-04-07 19:40:11 -0400

    create Island and Archipelago classes
    centralize even more of the logic for loading, displaying summaries,
    displaying plots, etc.

--- turnips/archipelago.py
+++ turnips/archipelago.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+import json
 import logging
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Generator, Optional
 
 import pydantic
 from pydantic import validator
@@ -9,6 +12,8 @@ from turnips.model import ModelEnum
 from turnips.meta import MetaModel
 from turnips.multi import MultiModel, BumpModels
 from turnips.ttime import TimePeriod
+
+from turnips.plots import plot_models_range, global_plot
 
 
 # pylint: disable=too-few-public-methods, no-self-use, no-self-argument
@@ -20,7 +25,7 @@ class Base(pydantic.BaseModel):
 
 class IslandModel(Base):
     previous_week: ModelEnum = ModelEnum.unknown
-    initial_week: Optional[bool] = False
+    initial_week: bool = False
     timeline: Dict[TimePeriod, Optional[int]]
 
     @validator('previous_week', pre=True)
@@ -44,68 +49,115 @@ class IslandModel(Base):
             raise ValueError("Cannot set initial_week = True when previous_week is set")
         return initial_week
 
+    @property
+    def base_price(self) -> Optional[int]:
+        return self.timeline.get(TimePeriod.Sunday_AM, None)
 
-class Archipelago(Base):
+
+class ArchipelagoModel(Base):
     islands: Dict[str, IslandModel]
 
 
-def load_json(filename: str) -> Archipelago:
-    return Archipelago.parse_file(filename)
+class Island:
+    def __init__(self, name: str, data: IslandModel):
+        self._name = name
+        self._data = data
+        self._models: MultiModel
+        self.process()
 
+    @property
+    def name(self) -> str:
+        return self._name
 
-def load_yaml(filename: str) -> Archipelago:
-    with open(filename, "r") as infile:
-        ydoc = yaml.safe_load(infile)
-    return Archipelago.parse_obj(ydoc)
+    @property
+    def previous_week(self) -> ModelEnum:
+        return self._data.previous_week
 
+    @property
+    def initial_week(self) -> bool:
+        return self._data.initial_week
 
-def process_data(data: Archipelago) -> Dict[str, MultiModel]:
-    island_models = {}
+    @property
+    def model_group(self) -> MultiModel:
+        return self._models
 
-    for name, idata in data.islands.items():
-        logging.info(f" == {name} island == ")
+    def process(self) -> None:
+        logging.info(f" == {self.name} island == ")
 
-        base = idata.timeline.get(TimePeriod.Sunday_AM, None)
-        models: MultiModel
-        if idata.initial_week:
-            models = BumpModels(base)
+        base = self._data.base_price
+        if self.initial_week:
+            self._models = BumpModels(base)
         else:
-            models = MetaModel.blank(base)
+            self._models = MetaModel.blank(base)
 
-        logging.info(f"  (%d models)  ", len(models))
+        logging.info(f"  (%d models)  ", len(self._models))
 
-        for time, price in idata.timeline.items():
+        for time, price in self._data.timeline.items():
             if price is None:
                 continue
             if time.value < TimePeriod.Monday_AM.value:
                 continue
             logging.info(f"[{time.name}]: fixing price @ {price}")
-            models.fix_price(time, price)
-        island_models[name] = models
-
-    return island_models
+            self._models.fix_price(time, price)
 
 
-def load(filename: str) -> Dict[str, MultiModel]:
-    if '.yml' in filename or '.yaml' in filename:
-        data = load_yaml(filename)
-    else:
-        data = load_json(filename)
+class Archipelago:
+    def __init__(self, data: ArchipelagoModel):
+        self._data = data
+        self._islands = {name: Island(name, idata) for name, idata
+                         in self._data.islands.items()}
 
-    return process_data(data)
+    @classmethod
+    def parse_obj(cls, obj: Dict[Any, Any]) -> Archipelago:
+        return cls(ArchipelagoModel.parse_obj(obj))
 
+    @classmethod
+    def load_json(cls, data: str) -> Archipelago:
+        jdata = json.loads(data)
+        return cls.parse_obj(jdata)
 
-def summary(island_models: Dict[str, MultiModel]) -> None:
-    for island, model in island_models.items():
-        print(f"{island}")
-        print('-' * len(island))
-        print('')
-        model.report()
-        print('')
+    @classmethod
+    def load_yaml(cls, data: str) -> Archipelago:
+        ydata = yaml.safe_load(data)
+        return cls.parse_obj(ydata)
 
-    # The initial doesn't matter here, it's ignored.
-    print('Archipelago Summary')
-    print('-' * 80)
-    archipelago = MetaModel(100, island_models.values())
-    archipelago.summary()
-    print('-' * 80)
+    @classmethod
+    def load_file(cls, filename: str) -> Archipelago:
+        if '.yml' in filename or '.yaml' in filename:
+            with open(filename, "r") as infile:
+                ydoc = yaml.safe_load(infile)
+                return cls.parse_obj(ydoc)
+
+        return cls(ArchipelagoModel.parse_file(filename))
+
+    @property
+    def groups(self) -> Generator[MultiModel, None, None]:
+        for island in self._islands.values():
+            yield island.model_group
+
+    @property
+    def islands(self) -> Generator[Island, None, None]:
+        for island in self._islands.values():
+            yield island
+
+    def summary(self) -> None:
+        for island in self.islands:
+            print(f"{island.name}")
+            print('-' * len(island.model_group))
+            print('')
+            island.model_group.report()
+            print('')
+
+        # The initial doesn't matter here, it's ignored.
+        print('Archipelago Summary')
+        print('-' * 80)
+        archipelago = MetaModel(-1, self.groups)
+        archipelago.summary()
+        print('-' * 80)
+
+    def plot(self) -> None:
+        for island in self.islands:
+            plot_models_range(island.name,
+                              list(island.model_group.models),
+                              island.previous_week)
+        global_plot(self.groups)
--- turnips/script.py
+++ turnips/script.py
@@ -11,6 +11,8 @@ def main() -> None:
     parser = argparse.ArgumentParser(description='Gaze into the runes')
     parser.add_argument('archipelago', metavar='archipelago-json',
                         help='JSON file with turnip price data for the week')
+    parser.add_argument('--plot', dest='plot', action='store_true',
+                        help='Draw some pretty graphs')
     parser.add_argument('--verbose', dest='verbose', action='store_true',
                         help='Turn on some informational messages')
     parser.add_argument('--debug', dest='debug', action='store_true',
@@ -22,8 +24,10 @@ def main() -> None:
     elif args.verbose:
         logging.basicConfig(level=logging.INFO)
 
-    island_models = archipelago.load(args.archipelago)
-    archipelago.summary(island_models)
+    arch = archipelago.Archipelago.load_file(args.archipelago)
+    arch.summary()
+    if args.plot:
+        arch.plot()
 
 
 if __name__ == '__main__':
