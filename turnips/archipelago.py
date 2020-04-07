9c44e1eaca2271093d9cd66480005efc02458ea3
nago <nago@trimex.us>
2020-04-07 17:29:00 -0400

    update script and samples

--- turnips/archipelago.py
+++ turnips/archipelago.py
@@ -1,11 +1,17 @@
-from typing import Dict, Optional
+import logging
+from typing import Any, Dict, Optional
 
 import pydantic
 from pydantic import validator
+import yaml
 
 from turnips.model import ModelEnum
+from turnips.meta import MetaModel
+from turnips.multi import MultiModel, BumpModels
 from turnips.ttime import TimePeriod
 
+
+# pylint: disable=too-few-public-methods, no-self-use, no-self-argument
 
 class Base(pydantic.BaseModel):
     class Config:
@@ -18,13 +24,13 @@ class IslandModel(Base):
     timeline: Dict[TimePeriod, Optional[int]]
 
     @validator('previous_week', pre=True)
-    def coerce_model(cls, value):
+    def coerce_model(cls, value: Any) -> Any:
         if isinstance(value, str):
             return ModelEnum[value.lower()]
         return value
 
     @validator('timeline', pre=True)
-    def normalize(cls, value):
+    def normalize(cls, value: Any) -> Any:
         if isinstance(value, Dict):
             return {
                 TimePeriod.normalize(key): price
@@ -33,10 +39,73 @@ class IslandModel(Base):
         return value
 
     @validator('initial_week')
-    def cajole(cls, initial_week: bool, values):
+    def cajole(cls, initial_week: bool, values: Dict[str, Any]) -> Any:
         if values['previous_week'] != ModelEnum.unknown and initial_week:
             raise ValueError("Cannot set initial_week = True when previous_week is set")
+        return initial_week
 
 
 class Archipelago(Base):
     islands: Dict[str, IslandModel]
+
+
+def load_json(filename: str) -> Archipelago:
+    return Archipelago.parse_file(filename)
+
+
+def load_yaml(filename: str) -> Archipelago:
+    with open(filename, "r") as infile:
+        ydoc = yaml.safe_load(infile)
+    return Archipelago.parse_obj(ydoc)
+
+
+def process_data(data: Archipelago) -> Dict[str, MultiModel]:
+    island_models = {}
+
+    for name, idata in data.islands.items():
+        logging.info(f" == {name} island == ")
+
+        base = idata.timeline.get(TimePeriod.Sunday_AM, None)
+        models: MultiModel
+        if idata.initial_week:
+            models = BumpModels(base)
+        else:
+            models = MetaModel.blank(base)
+
+        logging.info(f"  (%d models)  ", len(models))
+
+        for time, price in idata.timeline.items():
+            if price is None:
+                continue
+            if time.value < TimePeriod.Monday_AM.value:
+                continue
+            logging.info(f"[{time.name}]: fixing price @ {price}")
+            models.fix_price(time, price)
+        island_models[name] = models
+
+    return island_models
+
+
+def load(filename: str) -> Dict[str, MultiModel]:
+    if '.yml' in filename or '.yaml' in filename:
+        data = load_yaml(filename)
+    else:
+        data = load_json(filename)
+
+    return process_data(data)
+
+
+def summary(island_models: Dict[str, MultiModel]) -> None:
+    for island, model in island_models.items():
+        print(f"{island}")
+        print('-' * len(island))
+        print('')
+        model.report()
+        print('')
+
+    # The initial doesn't matter here, it's ignored.
+    print('Archipelago Summary')
+    print('-' * 80)
+    archipelago = MetaModel(100, island_models.values())
+    archipelago.summary()
+    print('-' * 80)
--- turnips/script.py
+++ turnips/script.py
@@ -3,11 +3,9 @@
 from __future__ import annotations
 
 import argparse
-import json
 import logging
 
-from turnips.meta import MetaModel
-
+from turnips import archipelago
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Gaze into the runes')
@@ -24,38 +22,8 @@ def main() -> None:
     elif args.verbose:
         logging.basicConfig(level=logging.INFO)
 
-    with open(args.archipelago) as infile:
-        pdata = json.load(infile)
-
-    island_models = {}
-    for name, data in pdata.items():
-        base = data.get('Sunday_AM', None)
-        data['Sunday_AM'] = None
-
-        logging.info(f" == {name} island == ")
-        model = MetaModel.blank(base)
-        logging.info(f"  (%d models)  ", len(model))
-
-        for time, price in data.items():
-            if price is None:
-                continue
-            logging.info(f"[{time}]: fixing price @ {price}")
-            model.fix_price(time, price)
-
-        island_models[name] = model
-
-    for island, model in island_models.items():
-        print(f"{island}")
-        print('-' * len(island))
-        print('')
-        model.report()
-        print('')
-
-    # The initial doesn't matter here, it's ignored.
-    print('-' * 80)
-    archipelago = MetaModel(100, island_models.values())
-    archipelago.summary()
-    print('-' * 80)
+    island_models = archipelago.load(args.archipelago)
+    archipelago.summary(island_models)
 
 
 if __name__ == '__main__':
